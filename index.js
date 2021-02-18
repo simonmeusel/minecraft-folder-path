b6de059de47cff927d17c455bea2026b3333e257
Simon Meusel <info@simonmeusel.de>
2021-02-18 15:02:57 +0100

    Update code formatting

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const path = require('path');
-const os = require('os');
+const path = require("path");
+const os = require("os");
 
 let folder = "";
 
 switch (os.type()) {
-    case 'Darwin':
-    folder = path.join(os.homedir(), '/Library/Application Support/minecraft');
-    break;
+    case "Darwin":
+        folder = path.join(
+            os.homedir(),
+            "/Library/Application Support/minecraft"
+        );
+        break;
 
-    case 'win32':
-    case 'Windows_NT':
-    folder = path.join(process.env.APPDATA, '.minecraft');
-    break;
+    case "win32":
+    case "Windows_NT":
+        folder = path.join(process.env.APPDATA, ".minecraft");
+        break;
 
     default:
-    folder = path.join(os.homedir(), '.minecraft');
-    break;
+        folder = path.join(os.homedir(), ".minecraft");
+        break;
 }
 
 module.exports = folder;
